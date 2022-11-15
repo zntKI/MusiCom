884ef1555ebe10c277afcdfacd8846b9f230a9ab
zntKI <kaloyanivanov@hotmail.com>
2022-11-15 21:07:36 +0200

    Extended the project structure

--- MusiCom.Infrastructure/Data/ApplicationDbContext.cs
+++ MusiCom.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace MusiCom.Data
+namespace MusiCom.Infrastructure.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- MusiCom/Program.cs
+++ MusiCom/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using MusiCom.Data;
+using MusiCom.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
