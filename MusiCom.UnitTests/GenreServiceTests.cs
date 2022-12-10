10885d5f74e6b893ad2d1397789011bfa94b3fb2
zntKI <kaloyanivanov@hotmail.com>
2022-12-10 19:44:37 +0200

    Added tests for GenreService and TagService

---
+++ MusiCom.UnitTests/GenreServiceTests.cs
@@ -0,0 +1,221 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusiCom.Core.Contracts.Admin;
+using MusiCom.Core.Models.Genre;
+using MusiCom.Core.Services.Admin;
+using MusiCom.Infrastructure.Data;
+using MusiCom.Infrastructure.Data.Common;
+using MusiCom.Infrastructure.Data.Entities.Events;
+using MusiCom.Infrastructure.Data.Entities.News;
+
+namespace MusiCom.UnitTests
+{
+    /// <summary>
+    /// Contains Unit Tests for GenreService
+    /// </summary>
+    [TestFixture]
+    public class GenreServiceTests
+    {
+        private ApplicationDbContext context;
+        private IGenreService genreService;
+        private IRepository repo;
+
+        /// <summary>
+        /// Sets up InMemoryDatabase
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("GenreDb")
+                .Options;
+
+            context = new ApplicationDbContext(contextOptions);
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            repo = new Repository(context);
+            genreService = new GenreService(repo);
+        }
+
+        /// <summary>
+        /// Asserts whether the method has taken the right Genre
+        /// </summary>
+        [Test]
+        public async Task TestGenreByIdAsyncInMemory()
+        {
+            await repo.AddRangeAsync(new List<Genre>()
+            {
+                new Genre() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("5872f3f2-4c44-476d-a67b-59a1bcda8b1f"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+            });
+            await repo.SaveChangesAsync();
+
+            var genre = await genreService.GetGenreByIdAsync(new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"));
+
+            Assert.That(new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Is.EqualTo(genre.Id));
+        }
+
+        /// <summary>
+        /// Asserts whether the method takes only the NotDeleted Genres
+        /// </summary>
+        [Test]
+        public async Task TestGetAllGenresAsyncNotDeletedInMemory()
+        {
+            await repo.AddRangeAsync(new List<Genre>()
+            {
+                new Genre() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "", IsDeleted = true, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("5872f3f2-4c44-476d-a67b-59a1bcda8b1f"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+            });
+            await repo.SaveChangesAsync();
+
+            var genres = await genreService.GetAllGenresAsync();
+
+            Assert.That(genres.Count(), Is.EqualTo(3));
+        }
+
+        /// <summary>
+        /// Asserts whether the method orders Genres correctly
+        /// </summary>
+        [Test]
+        public async Task TestGetAllGenresAsyncOrderedByDateInMemory()
+        {
+            await repo.AddRangeAsync(new List<Genre>()
+            {
+                new Genre() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "", IsDeleted = false, DateOfCreation = new DateTime(2019, 12, 12, 12, 12, 12), Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "", IsDeleted = false, DateOfCreation = new DateTime(2019, 12, 12, 12, 12, 13), Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "", IsDeleted = false, DateOfCreation = new DateTime(2019, 12, 12, 12, 12, 11), Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("5872f3f2-4c44-476d-a67b-59a1bcda8b1f"), Name = "", IsDeleted = false, DateOfCreation = new DateTime(2019, 12, 12, 12, 12, 10), Events = new List<Event>(), News = new List<New>() },
+            });
+            await repo.SaveChangesAsync();
+
+            IEnumerable<DateTime> ordered = new List<DateTime>()
+            {
+                new DateTime(2019, 12, 12, 12, 12, 13),
+                new DateTime(2019, 12, 12, 12, 12, 12),
+                new DateTime(2019, 12, 12, 12, 12, 11),
+                new DateTime(2019, 12, 12, 12, 12, 10)
+            };
+
+            List<DateTime> dates = new List<DateTime>();
+            var genres = await genreService.GetAllGenresAsync();
+            foreach (var genre in genres)
+            {
+                dates.Add(genre.DateOfCreation);
+            }
+
+            Assert.That(dates, Is.EqualTo(ordered));
+        }
+
+        /// <summary>
+        /// Asserts whether the method gets the Genre Names
+        /// </summary>
+        [Test]
+        public async Task TestGetAllGenreNamesAsync()
+        {
+            await repo.AddRangeAsync(new List<Genre>()
+            {
+                new Genre() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "Rock", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "Metal", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "Pop", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() }
+            });
+            await repo.SaveChangesAsync();
+
+            List<string> names = new List<string>()
+            {
+                "Rock", "Metal", "Pop"
+            };
+
+            var gNames = await genreService.GetAllGenreNamesAsync();
+
+            Assert.That(gNames, Is.EqualTo(names));
+        }
+
+        /// <summary>
+        /// Asserts wheter the method Edits a Genre correctly by comparing GenreName
+        /// </summary>
+        [Test]
+        public async Task TestEditGenreAsync()
+        {
+            await repo.AddRangeAsync(new List<Genre>()
+            {
+                new Genre() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "Rock", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "Metal", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "Pop", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() }
+            });
+            await repo.SaveChangesAsync();
+
+            GenreAllViewModel model = new GenreAllViewModel()
+            {
+                Name = "Folk",
+                DateOfCreation = DateTime.Now
+            };
+
+            await genreService.EditGenreAsync(new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), model);
+
+            var newGenre = await genreService.GetGenreByIdAsync(new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"));
+
+            Assert.That(newGenre.Name, Is.EqualTo("Folk"));
+        }
+
+        /// <summary>
+        /// Asserts that the method Deletes the Genre successfully
+        /// </summary>
+        [Test]
+        public async Task TestDeleteGenreAsync()
+        {
+            await repo.AddRangeAsync(new List<Genre>()
+            {
+                new Genre() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "Rock", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "Metal", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "Pop", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() }
+            });
+            await repo.SaveChangesAsync();
+
+            await genreService.DeleteGenreAsync(new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"));
+
+            var genres = await genreService.GetAllGenresAsync();
+
+            Assert.That(genres.Count(), Is.EqualTo(2));
+        }
+
+        /// <summary>
+        /// Asserts that the method Creates Genre successfully
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task TestCreateGenreAsync()
+        {
+            await repo.AddRangeAsync(new List<Genre>()
+            {
+                new Genre() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "Rock", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "Metal", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() },
+                new Genre() { Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "Pop", IsDeleted = false, DateOfCreation = DateTime.Now, Events = new List<Event>(), News = new List<New>() }
+            });
+            await repo.SaveChangesAsync();
+
+            var model = new GenreViewModel()
+            {
+                Name = "Jazz"
+            };
+            await genreService.CreateGenreAsync(model);
+
+            var genres = await genreService.GetAllGenresAsync();
+
+            Assert.That(genres.Count(), Is.EqualTo(4));
+        }
+
+        /// <summary>
+        /// Disposes the Context
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        { 
+           context.Dispose();
+        }
+    }
+}
---
+++ MusiCom.UnitTests/TagServiceTests.cs
@@ -0,0 +1,221 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusiCom.Core.Contracts;
+using MusiCom.Core.Models.Tag;
+using MusiCom.Core.Services;
+using MusiCom.Infrastructure.Data;
+using MusiCom.Infrastructure.Data.Common;
+using MusiCom.Infrastructure.Data.Entities.News;
+
+namespace MusiCom.UnitTests
+{
+    /// <summary>
+    /// Contains Unit Tests for TagService
+    /// </summary>
+    [TestFixture]
+    public class TagServiceTests
+    {
+        private ApplicationDbContext context;
+        private ITagService tagService;
+        private IRepository repo;
+
+        /// <summary>
+        /// Sets up InMemoryDatabase
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("TagDb")
+                .Options;
+
+            context = new ApplicationDbContext(contextOptions);
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            repo = new Repository(context);
+            tagService = new TagService(repo);
+        }
+
+        /// <summary>
+        /// Asserts whether the method has taken the right Tag
+        /// </summary>
+        [Test]
+        public async Task TestTagByIdAsyncInMemory()
+        {
+            await repo.AddRangeAsync(new List<Tag>()
+            {
+                new Tag() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now },
+                new Tag() { Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now },
+                new Tag() { Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now },
+                new Tag() { Id = new Guid("5872f3f2-4c44-476d-a67b-59a1bcda8b1f"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now },
+            });
+            await repo.SaveChangesAsync();
+
+            var tag = await tagService.GetTagByIdAsync(new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"));
+
+            Assert.That(new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Is.EqualTo(tag.Id));
+        }
+
+        /// <summary>
+        /// Asserts whether the method takes only the NotDeleted Tags
+        /// </summary>
+        [Test]
+        public async Task TestGetAllTagsAsyncNotDeletedInMemory()
+        {
+            await repo.AddRangeAsync(new List<Tag>()
+            {
+                new Tag() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now },
+                new Tag() { Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now },
+                new Tag() { Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "", IsDeleted = true, DateOfCreation = DateTime.Now },
+                new Tag() { Id = new Guid("5872f3f2-4c44-476d-a67b-59a1bcda8b1f"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now },
+            });
+            await repo.SaveChangesAsync();
+
+            var tags = await tagService.GetAllTagsAsync();
+
+            Assert.That(tags.Count(), Is.EqualTo(3));
+        }
+
+        /// <summary>
+        /// Asserts whether the method orders Tags correctly
+        /// </summary>
+        [Test]
+        public async Task TestGetAllTagsAsyncOrderedByDateInMemory()
+        {
+            await repo.AddRangeAsync(new List<Tag>()
+            {
+                new Tag() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "", IsDeleted = false, DateOfCreation = new DateTime(2019, 12, 12, 12, 12, 12) },
+                new Tag() { Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "", IsDeleted = false, DateOfCreation = new DateTime(2019, 12, 12, 12, 12, 13) },
+                new Tag() { Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "", IsDeleted = false, DateOfCreation = new DateTime(2019, 12, 12, 12, 12, 11) },
+                new Tag() { Id = new Guid("5872f3f2-4c44-476d-a67b-59a1bcda8b1f"), Name = "", IsDeleted = false, DateOfCreation = new DateTime(2019, 12, 12, 12, 12, 10) },
+            });
+            await repo.SaveChangesAsync();
+
+            IEnumerable<DateTime> ordered = new List<DateTime>()
+            {
+                new DateTime(2019, 12, 12, 12, 12, 13),
+                new DateTime(2019, 12, 12, 12, 12, 12),
+                new DateTime(2019, 12, 12, 12, 12, 11),
+                new DateTime(2019, 12, 12, 12, 12, 10)
+            };
+
+            List<DateTime> dates = new List<DateTime>();
+            var tags = await tagService.GetAllTagsAsync();
+            foreach (var genre in tags)
+            {
+                dates.Add(genre.DateOfCreation);
+            }
+
+            Assert.That(dates, Is.EqualTo(ordered));
+        }
+
+        /// <summary>
+        /// Asserts whether the method gets the Tag Names
+        /// </summary>
+        [Test]
+        public async Task TestGetAllTagNamesAsync()
+        {
+            await repo.AddRangeAsync(new List<Tag>()
+            {
+                new Tag() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "Yeah!", IsDeleted = false, DateOfCreation = DateTime.Now },
+                new Tag() {Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "Cool", IsDeleted = false, DateOfCreation = DateTime.Now },
+                new Tag() {Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "Very", IsDeleted = false, DateOfCreation = DateTime.Now }
+            });
+            await repo.SaveChangesAsync();
+
+            List<string> names = new List<string>()
+            {
+                "Yeah!", "Cool", "Very"
+            };
+
+            var tNames = await tagService.GetAllTagNamesAsync();
+
+            Assert.That(tNames, Is.EqualTo(names));
+        }
+
+        /// <summary>
+        /// Asserts wheter the method Edits a Tag correctly by comparing TagName
+        /// </summary>
+        [Test]
+        public async Task TestEditTagAsync()
+        {
+            await repo.AddRangeAsync(new List<Tag>()
+            {
+                new Tag() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "Yeah!", IsDeleted = false, DateOfCreation = DateTime.Now },
+                new Tag() { Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "Cool", IsDeleted = false, DateOfCreation = DateTime.Now },
+                new Tag() {Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "Very", IsDeleted = false, DateOfCreation = DateTime.Now }
+            });
+            await repo.SaveChangesAsync();
+
+            TagAllViewModel model = new TagAllViewModel()
+            {
+                Name = "Very Cool",
+                DateOfCreation = DateTime.Now
+            };
+
+            var tag = await tagService.GetTagByIdAsync(new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"));
+            await tagService.EditTagAsync(tag, model);
+
+            var newTag = await tagService.GetTagByIdAsync(new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"));
+
+            Assert.That(newTag.Name, Is.EqualTo("Very Cool"));
+        }
+
+        /// <summary>
+        /// Asserts that the method Deletes the Tag successfully
+        /// </summary>
+        [Test]
+        public async Task TestDeleteTagAsync()
+        {
+            await repo.AddRangeAsync(new List<Tag>()
+            {
+                new Tag() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now },
+                new Tag() {Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now },
+                new Tag() {Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now }
+            });
+            await repo.SaveChangesAsync();
+
+            var tag = await tagService.GetTagByIdAsync(new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"));
+            await tagService.DeleteTagAsync(tag);
+
+            var tags = await tagService.GetAllTagsAsync();
+
+            Assert.That(tags.Count(), Is.EqualTo(2));
+        }
+
+        /// <summary>
+        /// Asserts that the method Creates Tag successfully
+        /// </summary>
+        [Test]
+        public async Task TestCreateTagAsync()
+        {
+            await repo.AddRangeAsync(new List<Tag>()
+            {
+                new Tag() { Id = new Guid("7e2ec5de-ad4d-4900-925c-4147cbdf9569"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now },
+                new Tag() { Id = new Guid("89fedf3f-282c-43db-bfec-0ec8004cb3eb"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now },
+                new Tag() { Id = new Guid("68576c9f-be19-42eb-8b8b-f44205396b7f"), Name = "", IsDeleted = false, DateOfCreation = DateTime.Now }
+            });
+            await repo.SaveChangesAsync();
+
+            var model = new TagAllViewModel()
+            {
+                Name = "Jazz"
+            };
+            await tagService.CreateTagAsync(model);
+
+            var tags = await tagService.GetAllTagsAsync();
+
+            Assert.That(tags.Count(), Is.EqualTo(4));
+        }
+
+        /// <summary>
+        /// Disposes the Context
+        /// </summary>
+        [TearDown]
+        public void TearDown()
+        {
+            context.Dispose();
+        }
+    }
+}
